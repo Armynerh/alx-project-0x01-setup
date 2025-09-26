79b313d7168385a8330140443184b529fa04b16d
Armynerh <aimenah@outlook.com>
2025-09-26 15:18:07 -0400

    Modify User props

--- alx-project-0x01/pages/users/index.tsx
+++ alx-project-0x01/pages/users/index.tsx
@@ -1,4 +1,17 @@
+import UserCard from "@/components/common/PostCard";
+import { UserProps } from "@/interfaces";
 const Users: React.FC = () => {
   return <div>Users Page</div>;
 }
+export async function getStaticProps() {
+  const response = await fetch("https://jsonplaceholder.typicode.com/users")
+  const posts = await response.json()
+
+  return {
+    props: {
+      posts
+    }
+  }
+}
+
 export default Users;
