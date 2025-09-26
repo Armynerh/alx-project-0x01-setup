e0f717886f2f4fc8a1dfd8245890fbda669864e8
Armynerh <aimenah@outlook.com>
2025-09-26 14:46:30 -0400

    Create task setup

---
+++ alx-project-0x01/components/layout/Footer.tsx
@@ -0,0 +1,8 @@
+const Footer: React.FC = () => {
+  return (
+    <footer className="w-full py-4 bg-gray-800 text-white text-center">
+      <p>&copy; 2024 My Company. All rights reserved.</p>
+    </footer>
+  )
+}
+export default Footer;
